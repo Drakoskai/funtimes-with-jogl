050e7c344aee6c3026eeddbc2f3ed9effae371ab
Drakos <>
2016-04-14 19:53:33 -0400

    added renderable/prerenderable interfaces.  migrated more gl control into shape class.

---
+++ src/main/java/com/drakos/Prerenderable.java
@@ -0,0 +1,10 @@
+package com.drakos;
+
+/**
+ *
+ * @author Drakos
+ */
+public interface Prerenderable {
+
+    public void prerender(float dt);
+}
---
+++ src/main/java/com/drakos/Renderable.java
@@ -0,0 +1,10 @@
+package com.drakos;
+
+/**
+ *
+ * @author Drakos
+ */
+public interface Renderable {
+
+    public void render();
+}
